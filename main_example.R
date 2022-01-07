78460cfc066ec58239b031841ce3ba4fced76c66
zongnanhu <zongnanhu@163.com>
2022-01-07 13:11:20 +0800

    test: 更新测试代码

--- main_example.R
+++ main_example.R
@@ -2,8 +2,8 @@
 
 source("./rfm.R")
 
-
-for(i in 1:12){
+s <- Sys.time()
+for(i in 1:1){
     vlist_path <- file.path("./example",
                             paste("vlist", i, ".csv", sep = ""))
     elist_path <- file.path("./example",
@@ -11,8 +11,38 @@ for(i in 1:12){
     outimg_path <- file.path(".", paste("img", i, sep = ""))
     vtable <- read_vtable(vlist_path)
     etable <- read_etable(elist_path)
-    table1 <- init_status_table(vtable, 800)
+    table1 <- init_status_table(vtable, 100)
     res <- main_simulate(table1, vtable, etable,
-                         update_v_status, 500)
-    plot_simulate(res, outimg_path, all = FALSE)
+                         update_s_table, 10)
+    # plot_simulate(res, outimg_path, all = FALSE)
 }
+e <- Sys.time()
+print(e-s)
+
+
+s <- Sys.time()
+con <- dbConnect(RSQLite::SQLite(), dbname = "./test.sqlite")
+dbListTables(con)
+if(dbExistsTable(con, "res")){
+    dbExecute(con, "DROP TABLE res;")
+}
+dbWriteTable(con, "res",
+             data.frame(aid = integer(0),
+                        vid = numeric(0),
+                        time = numeric(0),
+                        rep = numeric(0)))
+for(i in 1:1){
+    vlist_path <- file.path("./example",
+                            paste("vlist", i, ".csv", sep = ""))
+    elist_path <- file.path("./example",
+                            paste("elist", i, ".csv", sep = ""))
+    vtable <- read_vtable(vlist_path)
+    etable <- read_etable(elist_path)
+    table1 <- init_status_table(vtable, 100)
+    main_simulate_db(con, "res",
+                     table1, vtable, etable,
+                     update_s_table, times = 10, rep = 10)
+}
+dbDisconnect(con)
+e <- Sys.time()
+print(e-s)
